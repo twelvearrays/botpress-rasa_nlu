64bd8daa78f2f0aa02ef3a5ef35249de186d8f47
Matt Schulz <twelvearrays@rebootthefuture.com>
2017-07-05 08:25:48 -0500

    Moved config and setRasaClient after bp.middlewares.register

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ const incomingMiddleware = (event, next) => {
 
   if (event.type === 'message') {
     service(event.text)
-      .then(({data}) => {
+      .then(({
+        data
+      }) => {
         event.rasa_nlu = data
         next()
       })
@@ -27,8 +29,7 @@ const incomingMiddleware = (event, next) => {
           var errMessage = err.response.data.error;
           event.bp.logger.error('botpress-rasa_nlu', 'Status', errStatus, '- Could not process incoming text:', errMessage);
           next()
-        }
-        else {
+        } else {
           event.bp.logger.error('botpress-rasa_nlu', 'Error code:', err.code, '- Check your Rasa server address on module options');
           next()
         }
@@ -42,14 +43,16 @@ const incomingMiddleware = (event, next) => {
 module.exports = {
 
   config: {
-    address: { type: 'string', default: 'http://localhost:5000', env: 'RASA_ADDRESS' },
+    address: {
+      type: 'string',
+      default: 'http://localhost:5000',
+      env: 'RASA_ADDRESS'
+    },
   },
 
-  init: async function(bp, configurator) {
+  init: async function (bp, configurator) {
 
-    config = await configurator.loadAll()
 
-    setRasaClient()
 
     bp.middlewares.register({
       name: 'rasa_nlu.incoming',
@@ -59,19 +62,27 @@ module.exports = {
       order: 10,
       description: 'Process natural language in the form of text. Structured data with an action and parameters for that action is injected in the incoming message event.'
     })
+
+    config = await configurator.loadAll()
+
+    setRasaClient()
   },
 
-  ready: async function(bp, configurator) {
+  ready: async function (bp, configurator) {
 
     const router = bp.getRouter('botpress-rasa_nlu')
 
-    router.get('/config', async (req, res) => {
+    router.get('/config', async(req, res) => {
       res.send(await configurator.loadAll())
     })
 
-    router.post('/config', async (req, res) => {
-      const { address } = req.body
-      await configurator.saveAll({ address })
+    router.post('/config', async(req, res) => {
+      const {
+        address
+      } = req.body
+      await configurator.saveAll({
+        address
+      })
       config = await configurator.loadAll()
       setRasaClient()
       res.sendStatus(200)
